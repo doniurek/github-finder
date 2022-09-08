f6a71fc1906f2b899a3ff5cef413eb81f32bfb58
doniurek <aldonaglebicka@gmail.com>
2022-09-09 00:57:14 +0200

    User Profile Top

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import NotFound from "./pages/NotFound"
-import User from "./pages/User"
 import Navbar from "./components/layout/Navbar"
 import Footer from "./components/layout/Footer"
 import Alert from "./components/layout/Alert"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import User from "./pages/User"
+import NotFound from "./pages/NotFound"
 import { GithubProvider } from "./context/github/GithubContext"
 import { AlertProvider } from "./context/alert/AlertContext"
 
@@ -18,11 +18,18 @@ function App() {
             <Navbar />
 
             <main className="container mx-auto px-3 pb-12">
-              <Alert />
               <Routes>
-                <Route path="/" element={<Home />} />
+                <Route
+                  path="/" 
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
                 <Route path="/about" element={<About />} />
-                <Route path="/user/:login" element={User} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/notfound" element={<NotFound />} />
                 <Route path="/*" element={<NotFound />} />
               </Routes>
