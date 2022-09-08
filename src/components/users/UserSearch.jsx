9bd2abdf3d4f28b41b5d5b99d6cc2f4f0083721b
doniurek <aldonaglebicka@gmail.com>
2022-09-08 19:41:30 +0200

    Alert Component

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,18 +1,20 @@
 import { useState, useContext } from "react"
 import GithubContext from "../../context/github/GithubContext"
+import AlertContext from "../../context/alert/AlertContext"
 
 function UserSearch() {
   const [text, setText] = useState("")
 
   const {users, searchUsers, clearUsers} = useContext(GithubContext)
+  const {setAlert} = useContext(AlertContext)
 
   const handleChange = (event) => setText(event.target.value)
 
   const handleSubmit = (event) => {
     event.preventDefault()
 
-    if(text === ""){
-      alert("Please enter something")
+    if(text === "") {
+      setAlert("Please enter something", "error")
     } else {
       searchUsers(text)
 
--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -17,7 +17,7 @@ export const AlertProvider = ({children}) => {
     setTimeout(() => dispatch({type: "REMOVE_ALERT"}), 3000)
   }
 
-  return <AlertContext.Provider value={{alert: state}}>
+  return <AlertContext.Provider value={{ alert: state, setAlert }}>
     {children}
   </AlertContext.Provider>
 }
